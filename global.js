58772f605c7030d177e4fc6662b63555e5dd6214
Colin Smith <colins@shawmutprinting.com>
2016-06-09 17:00:04 -0400

    Added quick little test node files

---
+++ global.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+
+//var hello = "Hello World from Node js";
+
+
+console.log(`Rock on World from ${path.basename(__filename)}`);
+
+//console.log(__dirname);
+//console.log(__filename);
+
