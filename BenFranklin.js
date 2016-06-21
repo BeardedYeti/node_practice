645005a16c47aee84dc970e2959d56f471a3c196
BeardedYeti <cmsgoboston41@gmail.com>
2016-06-21 12:53:05 -0400

    Person.js is it's own template now which can be required

--- BenFranklin.js
+++ BenFranklin.js
@@ -1,23 +1,26 @@
-var EventEmitter = require('events').EventEmitter;
-//Utilities module has an inherits function, and it's a way that wee can add a object to the prototype of an existing object.
-var util = require('util');
-//Person Constructor
-var Person = function(name) {
-	this.name = name;
-};
 
-//Person inherits the EventEmitter
-util.inherits(Person, EventEmitter);
-//New Object name Ben Franklin
+//Requires Person.js
+var Person = require("./lib/Person");
+
+//Creating new Person objects
 var ben = new Person("Ben Franklin");
+var george = new Person("George Washington");
+
 //Listens for when the object emits 'speak' and then passes 'said' to the function
 ben.on('speak', function(said) {
 
 	console.log(`${this.name}: ${said}`);
 
 });
+
+george.on('speak', function(said) {
+
+	console.log(`${this.name} -> ${said}`);
+
+});
+
 //ben the object emits speak and passes what he said
 ben.emit('speak', "You may delay, but time will not.");
-
+george.emit('speak', "It is far better to be alone, thsn to be in bad company.");
 
 
